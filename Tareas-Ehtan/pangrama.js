7b3d01f7ac4c90c2b3e9e9fec279844866fc9a19
= <=>
2024-05-07 12:58:45 -0600

    Added pangram homework

--- Tareas-Ehtan/pangrama.js
+++ Tareas-Ehtan/pangrama.js
@@ -1,17 +1,2 @@
 const pangram = (text) => new Set(text.toLowerCase().replace(/[^a-z]/g, '')).size === 26;
 console.log(pangram("abcdefghijklmnñopqrstuvwxyz"));
-
-function cosa(text) {
-  const abdc = "abcdefghijklmnñopqrstuvwxyz"
-  let a = true
-  const texta = text.split("")
-
-  abdc.split("").map(letra => {
-    if (!texta.some(letraCad => letra === letraCad)) {
-      return a = false
-    }
-  })
-  return a
-}
-
-console.log(cosa("abcdefghijklmtuvwxyzm"));
